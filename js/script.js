09edec1f2b95393e4f447c942c0059091457ad45
Marcos <marcintosh87@gmail.com>
2021-11-16 12:41:29 -0500

    removed repetative array, added .map to renderCard

--- js/script.js
+++ js/script.js
@@ -23,56 +23,24 @@ window.addEventListener("DOMContentLoaded", (event) => {
       .then((res) => res.json())
       .then((cocktail) => {
         let {
-          drinks: [
-            {
-              strDrink,
-              strDrinkThumb,
-              strIBA,
-              strIngredient1,
-              strIngredient2,
-              strIngredient3,
-              strIngredient4,
-              strIngredient5,
-              strIngredient6,
-              strIngredient7,
-              strIngredient8,
-              strIngredient9,
-              strIngredient10,
-              strIngredient11,
-              strIngredient12,
-              strIngredient13,
-              strIngredient14,
-              strIngredient15,
-            },
-          ],
+          drinks: [{}],
         } = cocktail;
         //dont touch
         cocktail.drinks.forEach(renderCard);
         //dont touch
-        console.log(cocktail.drinks);
+        console.log(cocktail);
       });
   }
 
   // measure15();
   function renderCard(cocktail) {
-    console.log(cocktail);
-    const ing = [
-      cocktail.strMeasure1 + cocktail.strIngredient1,
-      cocktail.strMeasure2 + cocktail.strIngredient2,
-      cocktail.strMeasure3 + cocktail.strIngredient3,
-      cocktail.strMeasure4 + cocktail.strIngredient4,
-      cocktail.strMeasure5 + cocktail.strIngredient5,
-      cocktail.strMeasure6 + cocktail.strIngredient6,
-      cocktail.strMeasure7 + cocktail.strIngredient7,
-      cocktail.strMeasure8 + cocktail.strIngredient8,
-      cocktail.strMeasure9 + cocktail.strIngredient9,
-      cocktail.strMeasure10 + cocktail.strIngredient10,
-      cocktail.strMeasure11 + cocktail.strIngredient11,
-      cocktail.strMeasure12 + cocktail.strIngredient12,
-      cocktail.strMeasure13 + cocktail.strIngredient13,
-      cocktail.strMeasure14 + cocktail.strIngredient14,
-      cocktail.strMeasure15 + cocktail.strIngredient15,
-    ];
+    const ing = Object.entries(cocktail)
+      .slice(17, 32)
+      .map((entry) => entry[1]);
+    const measure = Object.entries(cocktail)
+      .slice(32, 47)
+      .map((entry) => entry[1]);
+    const measureIng = measure.map((value, index) => value + ing[index]);
 
     //creation
     const cardBlock = document.createElement("div");
@@ -122,12 +90,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
     modalbody.style.padding = "3rem";
     modalIngHeader.textContent = "Ingredients";
     modalIngredients.classList.add("ingredients-list");
-    // modalIngredientsLi.textContent = measure[0] + ing[0];
-    ing.forEach((item) => {
+    //Ingredients
+    measureIng.forEach((item) => {
       if (item === 0) {
         return false;
       } else {
         const modalIngredientsLi = document.createElement("li");
+
         modalIngredientsLi.innerHTML = item.replace("null", "");
         modalIngredients.append(modalIngredientsLi);
       }
